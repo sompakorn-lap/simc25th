7225d8441a0a81b68d740b251ff2bc11318f338a
sompakorn-lap <122735033+sompakorn-lap@users.noreply.github.com>
2024-10-07 00:10:45 +0700

    update signup time to 07/10 11:59:59

--- client/src/pages/SignUp.page.tsx
+++ client/src/pages/SignUp.page.tsx
@@ -5,7 +5,7 @@ function SignUpPage() {
   return (
     <ProtectedRouteWithDatetime
       startDatetime="Fri Sep 13 2024 16:00:00 GMT+0700 (Indochina Time)"
-      endDatetime="Sun Oct 06 2024 23:59:59 GMT+0700 (Indochina Time)"
+      endDatetime="Mon Oct 07 2024 11:59:59 GMT+0700 (Indochina Time)"
     >
       <section>
         <h3>ลงทะเบียน</h3>
